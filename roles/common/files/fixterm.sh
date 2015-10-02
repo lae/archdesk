7194bc925570e311214815d9080623240dc9594e
Musee Ullah <musee.ullah@fireeye.com>
2015-10-02 09:32:54 -0700

    Various environment changes, sysstat config

---
+++ roles/common/files/fixterm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Managed by Ansible, modify it in the repo!
+if [[ "x$TERM" == "xscreen.rxvt" ]]; then export TERM="rxvt-256color"; fi
+if [[ "x$TERM" == "xscreen-256color-bce" ]]; then export TERM="screen-256color"; fi
---
+++ roles/common/files/history.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Managed by Ansible, modify it in the repo!
+HISTCONTROL=ignoreboth
+HISTSIZE=131072
+HISTFILESIZE=33554432
+HISTTIMEFORMAT="%Y.%m.%d %H:%M:%S "
+shopt -s histappend
